38ec142c768b85975a13e28989b83c763c219e51
AFakeman <anton_suslov@me.com>
2018-02-26 00:02:53 +0300

    Improvements

--- gunicorn/create_secret.sh
+++ gunicorn/create_secret.sh
@@ -1,3 +1,2 @@
-  RAND_STR=`head /dev/urandom | LC_CTYPE=C tr -dc A-Za-z0-9 | head -c 13`
-  echo SECRET_KEY=\'"$RAND_STR"\' > todolist/secret_settings.py
- 
+RAND_STR=`head /dev/urandom | LC_CTYPE=C tr -dc A-Za-z0-9 | head -c 13`
+echo SECRET_KEY=\'"$RAND_STR"\' > todolist/secret_settings.py 
